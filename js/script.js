88774360800bd104779a6a9957fd510c0d4fefc9
Mrsirinam <120685147+Mrsirinam@users.noreply.github.com>
2023-08-24 15:40:38 +0300

    add cities in select

--- js/script.js
+++ js/script.js
@@ -1,5 +1,47 @@
-const citySelect = document.querySelector("#city");
-const cityChoices = new Choices(citySelect, {
-  searchEnabled: false,
-  itemSelectText: "",
-});
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
+
+const getData = async (url, cbSuccess, cbError) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    cbSuccess(data);
+  } catch (err) {
+    cbError(err);
+  }
+};
+
+const init = () => {
+  const citySelect = document.querySelector("#city");
+  const cityChoices = new Choices(citySelect, {
+    itemSelectText: "",
+  });
+
+  //получаем данные с сервера (города)
+  getData(
+    `${API_URL}${LOCATION_URL}`,
+    (locationData) => {
+      const locations = locationData.map((location) => ({
+        value: location,
+      }));
+      cityChoices.setChoices(locations, "value", "label", false);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+};
+
+init();
+
+//тоже самое, что и getdata()
+// fetch(API_URL + LOCATION_URL)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
